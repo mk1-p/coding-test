6a842812c134ba1a21b8fe4dae9e30fc93b995e6
mk1-p <mk1.dev662@gmail.com>
2024-01-08 19:18:35 +0900

    CLN: 주석 추가

--- PythonCode/programmers/correct_parentheses.py
+++ PythonCode/programmers/correct_parentheses.py
@@ -15,17 +15,19 @@ def solution(strs):
 
     # O(n)
     for s in strs:
+        # 비교 문자열 변환, 괄호의 역방향으로 주어짐
         conv = ")" if "(" == s else "("
 
+        # 스택이 존재하는 경우에 문자열 비교 진행
         if stack:
             if stack[len(stack) - 1] == conv:
                 stack.pop()
             else:
                 stack.append(s)
+        # 스택이 존재하지 않는다면 괄호 시작인 경우에만 넘어감
         else:
             if s == ")":
                 return False
-
             stack.append(s)
 
     return False if stack else True
